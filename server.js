da3a36fc68059f79d39d8b5708e3c25cdd397bd5
diegopumacode <diegopumacode@gmail.com>
2021-11-20 22:30:23 -0500

    adding sqllite

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 
+const sqlite3 = require('sqlite3').verbose();
+
 const app = express()
+const db = new sqlite3.Database('./database/users.db');
+
+db.run('CREATE TABLE IF NOT EXISTS user(id TEXT, name TEXT, lastName TEXT, occupation TEXT, age INTEGER, status INTEGER)');
 
 let corsOptions = {
     origin: 'http://localhost:3000'
@@ -15,7 +20,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Initial Route
 app.get('/', (req, res) => {
-    res.send( "API running /_(--)_/" )
+    res.send("API running /_(--)_/")
 })
 
 const PORT = process.env.PORT || 8080

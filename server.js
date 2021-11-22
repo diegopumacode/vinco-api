e93643b4d40297b7cb89023144d1a1217b400ec8
diegopumacode <diegopumacode@gmail.com>
2021-11-21 21:51:42 -0500

    adding method[put]

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express()
 const db = require("./db.config.js")
 
-
 let corsOptions = {
     origin: 'http://localhost:3000'
 }
@@ -33,19 +32,35 @@ app.get("/user", (req, res, next) => {
 });
 
 app.post("/user/", (req, res, next) => {
-    var reqBody = req.body;
+    let reqBody = req.body;
     db.run(`INSERT INTO users(lastName, firstName, occupation, age, status) VALUES (?,?,?,?,?)`,
-        [reqBody.lastName, reqBody.firstName, reqBody.occupation, reqBody.status,reqBody.age],
+        [reqBody.lastName, reqBody.firstName, reqBody.occupation, reqBody.status, reqBody.age],
         function (err, result) {
             if (err) {
                 res.status(400).json({ "error": err.message })
                 return;
             }
             res.status(201).json({
-                "id": this.lastID
+                data: true,
+                id: this.lastID
             })
         });
 });
+
+app.put('/user/:id', function (req, res) {
+    let { lastName, firstName, occupation, age } = req.body;
+    db.serialize(() => {
+        db.run('UPDATE users SET lastName=? , firstName=? , occupation=? , age = ? WHERE id = ?',
+            [lastName, firstName, occupation, age, req.params.id], function (err) {
+                if (err) {
+                    res.send("Error while updating");
+                    return console.error(err.message);
+                }
+                res.send({ ok: true, message: "Entry updated successfully" });
+            });
+    });
+});
+
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
